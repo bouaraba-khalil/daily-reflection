f0fe14d927061e0d0c626a6926da6fb0642b9315
Khalil Bouaraba <bouaraba.khalil@gmail.com>
2025-02-18 16:40:11 +0100

    feat(daily): getall daily

--- server/api/v1/dailys/index.get.ts
+++ server/api/v1/dailys/index.get.ts
@@ -1,1 +1,11 @@
-export default defineEventHandler(async (e) => {});
+import { eq } from "drizzle-orm";
+import { db } from "~/server/database";
+import { dailysTable } from "~/server/database/schema/dailys";
+
+export default defineEventHandler(async (e) => {
+  const user_id = e.context.user.id;
+  return await db
+    .select()
+    .from(dailysTable)
+    .where(eq(dailysTable.user_id, user_id));
+});
