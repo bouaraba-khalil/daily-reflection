3b60443435ed9d997f97a73f39faaa41bc39168b
Khalil Bouaraba <bouaraba.khalil@gmail.com>
2025-02-18 16:42:01 +0100

    feat(daily): getall daily

--- server/api/v1/dailys/index.get.ts
+++ server/api/v1/dailys/index.get.ts
@@ -4,8 +4,11 @@ import { dailysTable } from "~/server/database/schema/dailys";
 
 export default defineEventHandler(async (e) => {
   const user_id = e.context.user.id;
-  return await db
+  const data = await db
     .select()
     .from(dailysTable)
     .where(eq(dailysTable.user_id, user_id));
+  const count = data.length;
+
+  return { data, count };
 });
