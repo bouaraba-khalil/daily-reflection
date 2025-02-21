ebd55b238599ac506bb1fd8ad943d5136a5cea4f
khalil <bouaraba.khalil@gmail.com>
2025-02-21 21:37:16 +0100

    feat(appHeader): implement app header

--- components/AppHeader/AppHeader.vue
+++ components/AppHeader/AppHeader.vue
@@ -1,8 +1,23 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="grid grid-cols-[100px_1fr_100px] max-w-screen-xl m-auto">
-    <div>q</div>
-    <div>q</div>
-    <div>1</div>
+  <div class="grid grid-cols-[100px_1fr_100px] max-w-screen-xl m-auto pt-5">
+    <div></div>
+    <div class="flex gap-2 justify-center">
+      <h2 class="hover:text-primary transition hover:underline text-xl">Daily Reflection</h2>
+    </div>
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Avatar class="border-primary border">
+            <AvatarImage src="/profile/1.png" />
+            <AvatarFallback>LOL</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+    </div>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,8 @@
-<script setup lang="ts">
-import AppHeader from "~/components/AppHeader/AppHeader.vue";
-
-const { user, status } = useUserStore();
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
-    <div
-      v-if="status === 'pending'"
-      class="min-w-screen min-h-screen grid place-items-center"
-    >
-      <Loader />
-    </div>
-    <div
-      v-else-if="status === 'error'"
-      class="min-w-screen min-h-screen grid place-items-center"
-    >
-      retry please
-    </div>
-    <main v-else-if="status === 'success'" class="min-w-screen min-h-screen">
-      <AppHeader /> <slot />
-    </main>
+    <AppHeader />
+    <slot />
   </div>
 </template>
